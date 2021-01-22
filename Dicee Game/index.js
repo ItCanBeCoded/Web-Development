21abe60c4f07eff463ea17cdc92806c604654def
itcanbecoded <doherty.steve.j@gmail.com>
2021-01-22 07:21:59 -0600

    Changed dom for h1 to be updated

--- Dicee Game/index.js
+++ Dicee Game/index.js
@@ -5,6 +5,9 @@ window.onload = function rollTheDice(){
     var whichDie2 = "images/diceY.png";
     var dieChosen;
     var dieChosen2;
+    var winner1 = "Player 1 Wins";
+    var winner2 = "Player 2 Wins";
+    var tie = "It's a Tie!";
 
     diceResult = Math.ceil(6 * Math.random());
     diceResult2 = Math.ceil(6 * Math.random());
@@ -17,7 +20,16 @@ window.onload = function rollTheDice(){
 
     document.querySelector(".img1").setAttribute("src", dieChosen);
     document.querySelector(".img2").setAttribute("src", dieChosen2);
-      
+
+    if (diceResult > diceResult2){
+        document.querySelector("h1").innerHTML = winner1;
+    }
+    if (diceResult < diceResult2){
+        document.querySelector("h1").innerHTML = winner2;
+    }  
+    if (diceResult === diceResult2){
+        document.querySelector("h1").innerHTML = tie;
+    }
 
 
 }
