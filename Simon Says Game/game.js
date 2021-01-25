a5779fd713913f584b75b2acfc8621304c424468
itcanbecoded <doherty.steve.j@gmail.com>
2021-01-25 15:47:52 -0600

    simon says js

---
+++ Simon Says Game/game.js
@@ -0,0 +1,9 @@
+var buttonColors = ["red","blue","green","yellow"];
+
+function nextSequence() {
+    var randomNumber = Math.floor(4*Math.random());
+    console.log(randomNumber);
+}
+
+nextSequence();
+
