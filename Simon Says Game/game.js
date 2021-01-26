e56767eac7ddfbfc18f6e7f88a746dbd59a32d67
itcanbecoded <doherty.steve.j@gmail.com>
2021-01-26 17:06:08 -0600

    onclick adds to array

--- Simon Says Game/game.js
+++ Simon Says Game/game.js
@@ -1,14 +1,33 @@
 var gamePattern = [];
-
-var buttonColors = ["red","blue","green","yellow"];
-
+var randomNumber;
 var randomChosenColor;
+var audio;
+var userClickedPattern = [];
+
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+$(".btn").click( function(){
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+    nextSequence();
+});
+
 
 function nextSequence() {
-    var randomNumber = Math.floor(4*Math.random());
+
+    randomNumber = Math.floor(4 * Math.random());
     randomChosenColor = buttonColors[randomNumber];
-    console.log(randomChosenColor);
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+
+    audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
+
+
+    
 }
 
-nextSequence();
-
+    nextSequence();
