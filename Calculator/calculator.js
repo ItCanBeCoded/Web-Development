8351cb58111f84e632516903cfd09609be0e7dee
itcanbecoded <doherty.steve.j@gmail.com>
2021-02-03 15:40:43 -0600

    get weather data and display

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -18,9 +18,22 @@ app.post("/", function(req, res){
 
     var result = num1 + num2;
 
-    console.log(req.body.result);
 
     res.send("The result of the calculation is " + result);
+})
+
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post("/bmicalculator", function(req, res){
+
+    var height = Number(req.body.height);
+    var weight = Number(req.body.weight);
+
+    var bmi = (weight/(Math.pow(height, 2)))*703;
+    bmi = bmi.toFixed(1);
+    res.send("Your BMI is " + bmi);
 })
 
 
