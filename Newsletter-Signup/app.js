b759715686e51c142a55e869552194994f3982b7
itcanbecoded <doherty.steve.j@gmail.com>
2021-02-04 17:00:13 -0600

    heroku deploy

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,15 +1,74 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Everything above here is standard boilerplate for express/bodyparser projects
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", function(req, res){
+    
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        
+
+    ] };
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us7.api.mailchimp.com/3.0/lists/83d0766ee4/";
+    const options = {
+        method: "POST",
+        auth: "steve1:31d6997b4d5c6962108380dc9f245afd-us7"
+
+    }
+    const request = https.request(url, options, function(response){
+
+        if(response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+})
+
 app.listen(3000, function(){
     console.log("server listening on port 3000");
 })
+
+//API KEY
+//31d6997b4d5c6962108380dc9f245afd-us7
+
+//mailchimp list ID
+//83d0766ee4
