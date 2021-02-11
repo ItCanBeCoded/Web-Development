d0c9d64534482645b56ee376d9eb4d3b9b3604a2
itcanbecoded <doherty.steve.j@gmail.com>
2021-02-11 15:18:43 -0600

    add collection

--- fruits-project/app.js
+++ fruits-project/app.js
@@ -3,19 +3,54 @@ const { MongoClient } = require("mongodb");
 const uri =
   "mongodb://localhost:27017";
 // Create a new MongoClient
-const client = new MongoClient(uri);
-const dbName = "fruitsDB";
+const client = new MongoClient(uri, { useUnifiedTopology: true});
+
 async function run() {
   try {
     // Connect the client to the server
     await client.connect();
     // Establish and verify connection
+
+    const database = client.db("fruitsDB");
+    const collection = database.collection("fruits");
     
-    await client.db(dbName).command({ ping: 1 });
-    console.log("Connected successfully to server");
+    
+
+    const fruitDocuments = [
+      {
+        name: "Orange",
+        score: 6,
+        review: "its juicy"
+      },
+      {
+        name: "Banana",
+        score: 9,
+        review: "Very tasty"
+      },
+      {
+      name: "Orange",
+      score: 6,
+      review: "its juicy"
+      }
+    ];
+
+    const options = { ordered: true };
+    
+    const result = await collection.insertMany(fruitDocuments, options);
+    
+    console.log("${result.insertedCount} documents were inserted")
+
+
   } finally {
     // Ensures that the client will close when you finish/error
+    
     await client.close();
+    
+    
   }
 }
 run().catch(console.dir);
+
+
+
+
