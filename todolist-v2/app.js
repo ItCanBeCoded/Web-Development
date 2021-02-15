57338f7c054c344a32d15afa9eb79dc418f8fe47
itcanbecoded <doherty.steve.j@gmail.com>
2021-02-14 21:50:22 -0600

    heroku deploy

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-steve:RmNrxPg786VRV5y@clusterduck.u9gyb.gcp.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
@@ -129,6 +129,11 @@ app.post("/delete", function(req, res){
 app.get("/about", function(req, res){
   res.render("about");
 });
+
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
