5952181fd8ef0bce98095f24db10e37d38bfe670
itcanbecoded <doherty.steve.j@gmail.com>
2021-02-25 08:13:02 -0600

    find or create

--- Authenticate/app.js
+++ Authenticate/app.js
@@ -47,8 +47,15 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(user, done){
+    User.findById(id, function(err, user){
+        done(err, user);
+    });
+});
 
 passport.use(new GoogleStrategy({
     clientID:     process.env.CLIENT_ID,
